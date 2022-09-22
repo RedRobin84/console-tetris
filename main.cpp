edc94362003cd31322bf894c6376acd3aa57add2
Martin Červenka <martin.cervenka@rohlik.cz>
2022-09-23 00:10:13 +0200

    position rendering

--- main.cpp
+++ main.cpp
@@ -47,10 +47,10 @@ const int MIN = 1;
 const int MAX = 10;
 
 const Point UPPER_LEFT_BORDER {MIN, MIN}; 
-const Point UPPER_RIGHT_BORDER {MIN, MAX}; 
-const Point BOTTOM_LEFT_BORDER {MAX, MIN}; 
+const Point UPPER_RIGHT_BORDER {MAX, MIN}; 
+const Point BOTTOM_LEFT_BORDER {MIN, MAX}; 
 const Point BOTTOM_RIGHT_BORDER {MAX, MAX}; 
-const Point TOP_CENTER {MIN, MAX / 2}; 
+const Point TOP_CENTER {MAX / 2, MIN}; 
 
 const int ROWS {MAX};
 const int COLUMNS {MAX};
@@ -59,18 +59,18 @@ const char* DEFAULT_CHAR = "O";
 
 const Piece L {
 	std::array<Rotation, ROTATION_NUMBER>{
-		std::array<Point, PIECE_POINT_NUMBER>{Point{1,1}, Point{2,1}, Point{3,1}, Point{3, 2}},
-		std::array<Point, PIECE_POINT_NUMBER>{Point{1,1}, Point{2,1}, Point{3,1}, Point{3, 2}},
-		std::array<Point, PIECE_POINT_NUMBER>{Point{1,1}, Point{2,1}, Point{3,1}, Point{3, 2}},
-		std::array<Point, PIECE_POINT_NUMBER>{Point{1,1}, Point{2,1}, Point{3,1}, Point{3, 2}},
+		std::array<Point, PIECE_POINT_NUMBER>{Point{0,0}, Point{0,1}, Point{0,2}, Point{1, 2}},
+		std::array<Point, PIECE_POINT_NUMBER>{Point{0,2}, Point{1,2}, Point{2,2}, Point{2, 1}},
+		std::array<Point, PIECE_POINT_NUMBER>{Point{2,2}, Point{2,1}, Point{2,0}, Point{1, 0}},
+		std::array<Point, PIECE_POINT_NUMBER>{Point{2,0}, Point{1,0}, Point{0,0}, Point{0, 1}},
 	}
 };
 
-Point current_piece_pos {TOP_CENTER};
+const Point starting_position {TOP_CENTER};
 
 
 void draw_on_pos(const Point& point) {
-	mvprintw(point.x, point.y, DEFAULT_CHAR);
+	mvprintw(point.y, point.x, DEFAULT_CHAR);
 }
 
 int main() {
@@ -80,13 +80,20 @@ int main() {
     keypad(stdscr, TRUE);  // allow arrow keys
     timeout(0);            // no blocking on getch()
     curs_set(0);           // set the cursor to invisible
-    std::string render_result {play_field.substr(1)};
-    printw(render_result.c_str());
-    std::for_each(L.rotations[0].coordinates.begin(), L.rotations[0].coordinates.end(), draw_on_pos);
-    draw_on_pos(current_piece_pos);
-    refresh();
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000)); 
-    clear();
+    Point current_pos = starting_position;
+    int i = 0;
+    while(i < L.rotations.size()) {
+    	std::string render_result {play_field.substr(1)};
+    	printw(render_result.c_str());
+    	std::for_each(L.rotations[i].coordinates.begin(), L.rotations[i].coordinates.end(), [&current_pos](const Point& p) {
+			draw_on_pos({p.x + current_pos.x, p.y + current_pos.y});
+			});
+    	refresh();
+    	std::this_thread::sleep_for(std::chrono::milliseconds(1000)); 
+   	clear();
+	i++;
+	current_pos.y++;
+    }
     endwin();
     return 0;
 }
