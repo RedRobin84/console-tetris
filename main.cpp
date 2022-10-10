d14cbd42a9b8f8099cc0b15f667e53557cb324a5
Martin Červenka <martin.cervenka@rohlik.cz>
2022-10-10 23:40:07 +0200

    add boolean flag game loop add key press switch torso quitting with q key

--- main.cpp
+++ main.cpp
@@ -29,17 +29,31 @@ int main() {
     timeout(0);            // no blocking on getch()
     curs_set(0);           // set the cursor to invisible
     Point current_pos = starting_position;
-    int i = 0;
-    while(i < L.rotations.size()) {
-    	std::string render_result {play_field.substr(1)};
+    bool running = true;
+    const auto render_result {play_field.substr(1)};
+
+    while(running) {
+    	switch (getch()) {
+    		case KEY_LEFT:
+			break;
+		case KEY_RIGHT:
+			break;
+		case KEY_UP:
+			break;
+		case KEY_DOWN:
+			break;
+		case 'q':
+			running = false;
+			continue;
+	}
+
     	printw(render_result.c_str());
-    	std::for_each(L.rotations[i].coordinates.begin(), L.rotations[i].coordinates.end(), [&current_pos](const Point& p) {
-			draw_on_pos({p.x + current_pos.x, p.y + current_pos.y});
-			});
+	std::for_each(L.current_rotation->coordinates.cbegin(), L.current_rotation->coordinates.cend(), [&current_pos](const Point& p) {
+                        draw_on_pos({p.x + current_pos.x, p.y + current_pos.y});
+                        });
     	refresh();
     	std::this_thread::sleep_for(std::chrono::milliseconds(1000)); 
    	clear();
-	i++;
 	current_pos.y++;
     }
     endwin();
