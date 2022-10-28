244d28d2c51315d7356b8be2b89b180a36e9344d
Martin Červenka <martin.cervenka@rohlik.cz>
2022-10-29 00:32:37 +0200

    formatting rfc

--- main.hpp
+++ main.hpp
@@ -22,12 +22,12 @@ struct Rotation
 	const std::array<Point, PIECE_POINT_NUMBER> coordinates;
 
 	std::set<int> get_line_set() const {
-		std::set<int> lines;
-		std::for_each(coordinates.cbegin(), coordinates.cend(),
-				[&lines](const Point& p) {
-				lines.insert(p.y);
-				});
-		return lines;
+	    std::set<int> lines;
+	    std::for_each(coordinates.cbegin(), coordinates.cend(),
+		[&lines](const Point& p) {
+		lines.insert(p.y);
+		});
+	    return lines;
 	}
 };
 
