c01e4374c53b94651d594720ad6f5c24cb959c7e
Martin Červenka <martin.cervenka@rohlik.cz>
2022-11-15 12:46:29 +0100

    add test

--- tests/score_test.cpp
+++ tests/score_test.cpp
@@ -11,10 +11,25 @@ void init_test() {
     printf("FAILED. Score should be %s. But was %s.\n", EXPECTED_VALUE, score.get_score().c_str());
 }
 
+void update_score_test() {
+    printf("update_score_test: ");
+    const auto EXPECTED_VALUE = "12";
+    Score score{};
+    score.update_based_on(3);
+    score.update_based_on(4);
+    score.update_based_on(9);
+    if (score.get_score() == EXPECTED_VALUE) {
+	printf("PASSED.\n");
+	return;
+    }
+    printf("FAILED. Score should be %s. But was %s.\n", EXPECTED_VALUE, score.get_score().c_str());
+}
+
 auto main() -> int {
-    printf("*** Score TEST ***)\n\n");
+    printf("*** Score TEST ***\n\n");
 
     init_test(); 
+    update_score_test();
 
     printf("\n");
 }
