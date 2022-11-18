61d0b6c8f48b91be7df88786ec977f4baea7feb2
Martin Červenka <martin.cervenka@rohlik.cz>
2022-11-18 23:33:40 +0100

    add missing z and s pieces

--- pieces.hpp
+++ pieces.hpp
@@ -1,7 +1,18 @@
 #pragma once
 #include "piece.hpp"
 
-std::array<Piece, 5> pieces {
+/*
+ * L
+ * J
+ * T
+ * square
+ * I
+ * Z
+ * S
+*/
+const size_t NR_OF_PIECES = 7;
+
+std::array<Piece, NR_OF_PIECES> pieces {
 	Piece{
 		Rotation{Point{1, 0}, Point{1, 1}, Point{1, 2}, Point{2, 2}},
 		Rotation{Point{0, 1}, Point{1, 1}, Point{2, 1}, Point{0, 2}},
@@ -35,5 +46,19 @@ std::array<Piece, 5> pieces {
 		Rotation{Point{2, 0}, Point{2, 1}, Point{2, 2}, Point{2, 3}},
 		Rotation{Point{0, 2}, Point{1, 2}, Point{2, 2}, Point{3, 2}},
 		Rotation{Point{0, 0}, Point{0, 1}, Point{0, 2}, Point{0, 3}}
+	},
+
+	Piece{
+		Rotation{Point{0, 0}, Point{1, 0}, Point{1, 1}, Point{2, 1}},
+		Rotation{Point{2, 0}, Point{2, 1}, Point{1, 1}, Point{1, 2}},
+		Rotation{Point{0, 1}, Point{1, 1}, Point{1, 2}, Point{2, 2}},
+		Rotation{Point{1, 0}, Point{0, 1}, Point{1, 1}, Point{0, 2}}
+	},
+
+	Piece{
+		Rotation{Point{1, 0}, Point{2, 0}, Point{1, 1}, Point{0, 1}},
+		Rotation{Point{1, 0}, Point{1, 1}, Point{2, 1}, Point{2, 2}},
+		Rotation{Point{1, 1}, Point{2, 1}, Point{0, 2}, Point{1, 2}},
+		Rotation{Point{0, 0}, Point{0, 1}, Point{1, 1}, Point{1, 2}}
 	}
 };
