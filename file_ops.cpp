5d35b0bfd23fa724bf6478063833789077c2029c
Martin Červenka <martin.cervenka@rohlik.cz>
2022-12-11 19:48:14 +0100

    refactor to optional

--- file_ops.cpp
+++ file_ops.cpp
@@ -1,30 +1,29 @@
 #include "file_ops.hpp"
 #include <fstream>
 
-auto read_score_from_binary_file(const char* file_path) -> const int {
-    int score{0};
+auto is_file_empty(std::ifstream& file) -> bool {
+    return file.peek() == std::ifstream::traits_type::eof();
+}
+
+auto read_score_from_binary_file(const char* file_path) -> std::optional<int> {
     std::ifstream file (file_path, std::ios::in | std::ios::binary);
     if (not file.is_open()) {
-	printf("ERROR: read_score_from_binary_file: unable to open file\n");
-	return score;
+	return std::nullopt;
     }
-    if (file.peek() == std::ifstream::traits_type::eof()) {
-	printf("read_score_from_binary_file: Empty file, returning default score\n");
-	file.close();
-	return score;
+    if (is_file_empty(file)) {
+	return 0;
     }
+    int score{0};
     file.read((char*) &score, sizeof(score));
-    file.close();
     return score;
 }
 
 void write_score_to_binary_file(const int number, const char* file_path) {
-	std::ofstream file (file_path, std::ios::out | std::ios::binary);
-	if (not file.is_open()) {
-	    printf("ERROR: write_int_to_binary_file: unable to open file\n");
-	    return;
-	}
-	file.write((char*) &number, sizeof(number));
-	file.close();
+    std::ofstream file (file_path, std::ios::out | std::ios::binary);
+    if (not file.is_open()) {
+	printf("ERROR: write_int_to_binary_file: unable to open file\n");
+	return;
+    }
+    file.write((char*) &number, sizeof(number));
 }
 
